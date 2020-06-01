baed3ee8a4d91d7ab3a8919dd7b75496fd0ad4d0
abuyako <abuyako@gmail.com>
2020-06-01 16:15:26 +0300

    add then as we all know the fetch return promise

--- fetch-API-Users/app.js
+++ fetch-API-Users/app.js
@@ -10,3 +10,12 @@ const ul = document.getElementById('authors');
 const url = 'https://randomuser.me/api/?results=10';
 
 fetch(url)
+.then((res) => res.json())
+.then((data) => {
+    let authors = data.results;
+    return authors.map((author) => {
+        let li = createNode('li'),
+            img = createNode('img'),
+            span = createNode('span');
+    })
+})
